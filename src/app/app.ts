ce85db50a44b367dc6b0a52ec3b8f123d69b5c05
jojartbence <jojart.bence96@gmail.com>
2025-12-14 21:36:43 +0100

    Let AI generate a basic website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Services } from './components/services/services';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Header,
+    Home,
+    About,
+    Services,
+    Contact,
+    Footer,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('barbi-website');
-}
+export class App {}
